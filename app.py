c8c13617964763c1ff05a0dd1d45e3ae79724517
syasin <syasinahya@gmail.com>
2018-08-20 17:33:54 +0900

    add templates

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-# app.py
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def index():
-    return "Hello World!"
+    # 「templates/index.html」のテンプレートを使う
+    # 「message」という変数に"Hello"と代入した状態で、テンプレート内で使う
+    return render_template('index.html', message="Hello")
 
 if __name__ == "__main__":
     app.run()
